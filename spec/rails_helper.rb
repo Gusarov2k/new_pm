bcfdc2441c6228501ae14e1f9db55b55bbb90147
gusarov2k <motoface@yandex.ru>
2018-07-28 14:07:51 +0300

    fix bag config sholda-matche

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -55,26 +55,15 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
-  config.include(Shoulda::Matchers::ActiveModel, type: :model)
-  config.include(Shoulda::Matchers::ActiveRecord, type: :model)
+require 'shoulda/matchers'
   
   Shoulda::Matchers.configure do |config|
-    config.integrate do |with|
-      # Choose a test framework:
-      with.test_framework :rspec
-      with.test_framework :minitest
-      with.test_framework :minitest_4
-      with.test_framework :test_unit
-
-      # Choose one or more libraries:
-      with.library :active_record
-      with.library :active_model
-      with.library :action_controller
-      # Or, choose the following (which implies all of the above):
-      with.library :rails
-    end
+      config.integrate do |with|
+        with.test_framework :rspec
+        with.library :rails
+      end
   end
-
 end
 
+  
 
